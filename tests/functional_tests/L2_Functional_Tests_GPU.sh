edd82576250d6f8bc9553a1a03dae0b5f081d92c
oliver könig <okoenig@nvidia.com>
2025-06-13 13:40:09 +0000

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- tests/functional_tests/L2_Functional_Tests_GPU.sh
+++ tests/functional_tests/L2_Functional_Tests_GPU.sh
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUDA_VISIBLE_DEVICES="0,1" torchrun --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode -m pytest tests/functional_tests -v -m "not pleasefixme" --tb=short 
+CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.run --nproc_per_node=2 --nnodes=1 -m coverage run --data-file=/workspace/.coverage --source=/workspace/ --parallel-mode -m pytest tests/functional_tests -v -m "not pleasefixme" --tb=short
 coverage combine
