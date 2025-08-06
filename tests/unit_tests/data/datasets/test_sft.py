768895445ae5dd6c5e513925578d5339551094ad
dimapihtar <dpihtar@gmail.com>
2025-08-06 10:12:08 -0700

    fix torch init
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- tests/unit_tests/data/datasets/test_sft.py
+++ tests/unit_tests/data/datasets/test_sft.py
@@ -78,6 +78,7 @@ def get_gpt_sft(ensure_test_data, dataset_type="sft"):
 
 class TestDataGPTSFTDataset:
     def test_build_samples_mapping(self, ensure_test_data):
+        setup_megatron_distributed
         dataset = get_gpt_sft(ensure_test_data)
         dataset._build_samples_mapping()
 
