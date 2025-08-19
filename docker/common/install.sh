8d913051c008cd90101f6083b3bae8e4f72ab34f
oliver könig <okoenig@nvidia.com>
2025-08-19 12:44:38 +0000

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- docker/common/install.sh
+++ docker/common/install.sh
@@ -66,7 +66,7 @@ main() {
     apt-get install -y wget curl git cmake
 
     # Install CUDA
-    if [[ "$BASE_IMAGE" == "ubuntu" ]]; then
+    if [[ "$BASE_IMAGE" != "pytorch" ]]; then
         wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
         dpkg -i cuda-keyring_1.1-1_all.deb
         rm cuda-keyring_1.1-1_all.deb
