aa77a615994726d2ef52bc8b241e9a15cf2017e4
dimapihtar <dpihtar@gmail.com>
2025-09-01 04:21:11 -0700

    fix unit tests
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- tests/unit_tests/training/test_checkpointing.py
+++ tests/unit_tests/training/test_checkpointing.py
@@ -385,8 +385,12 @@ class TestSaveCheckpoint:
         mock_torch_save.return_value = None  # Mock file save
         mock_shutil_copy.return_value = None  # Mock file copy
 
+
         # Add wandb logger to state
         save_checkpoint_fixtures["mock_state"].wandb_logger = Mock()
+
+        save_checkpoint_fixtures["mock_state"].cfg.checkpoint.save_top_k = -1
+        save_checkpoint_fixtures["mock_state"].cfg.checkpoint.save_retain_interval = 1
 
         # Call save_checkpoint
         save_checkpoint(
