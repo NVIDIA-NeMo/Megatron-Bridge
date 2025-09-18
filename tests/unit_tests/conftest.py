849a360493aa52b96a5f8a1c58de251db7b7e1c2
gkollu <gkollu@nvidia.com>
2025-09-18 14:38:43 -0700

    lint
    Signed-off-by: gkollu <gkollu@nvidia.com>

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -51,6 +51,7 @@ def ensure_test_data(tmp_path_factory):
         try:
             # Download assets to data_path
             from tests.unit_tests.download_unit_tests_dataset import get_oldest_release_and_assets
+
             get_oldest_release_and_assets(assets_dir=str(data_path))
 
             logger.info("Test data downloaded successfully.")
