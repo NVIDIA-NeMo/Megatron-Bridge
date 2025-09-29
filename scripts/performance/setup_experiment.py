521766ff5d860a309c7db0adeb97fe163bf85a49
Sebastian Alberdi <salberdi@nvidia.com>
2025-09-28 22:34:08 -0700

    llmb edits
    Signed-off-by: Sebastian Alberdi <salberdi@nvidia.com>

--- scripts/performance/setup_experiment.py
+++ scripts/performance/setup_experiment.py
@@ -46,7 +46,8 @@ logger: logging.Logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     args, _ = parse_cli_args()
     exp_name = f"{args.model_name}_{args.model_size}_{args.domain}_{args.task}"
-    exp_name += "_bf16" if args.compute_dtype == "bf16" else f"_{args.compute_dtype}_{args.fp8_recipe}"
+    dtype = "bf16" if args.compute_dtype == "bf16" else f"{args.compute_dtype}_{args.fp8_recipe}"
+    exp_name += f"_{dtype}"
 
     if args.model_name in ["qwen3"] and args.model_size in ["30b_a3b", "235b_a22b"]:
         assert args.hf_token is not None, "HF token is required for Qwen3 tokenizer. NullTokenizer to be used soon."
@@ -135,5 +136,12 @@ if __name__ == "__main__":
         entrypoint="python",
         args=target_script_args,
     )
+
+    perf_matrix = yaml_overrides_omega["perf_matrix"][args.gpu][f"num_gpus_{args.num_gpus}"]
+    base_config = perf_matrix["common"]
+    extra_config = perf_matrix[dtype]
+    train_config = OmegaConf.merge(base_config, extra_config) if extra_config else base_config
+    exp_config = f"gpus{args.num_gpus}_tp{train_config["tp"]}_pp{train_config["pp"]}_cp{train_config["cp"]}_vp{train_config["vp"]}_ep{train_config["ep"]}_mbs{train_config["mbs"]}_gbs{train_config["gbs"]}"
+    exp_name = f"pretrain_{args.model_name}_{args.model_size}_{dtype}_{exp_config}"
 
     run.run(train_script, executor=executor, plugins=plugins, dryrun=args.dryrun, detach=True, name=exp_name)
