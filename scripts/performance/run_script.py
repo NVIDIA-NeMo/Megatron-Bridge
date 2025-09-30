607f3cc59c077652efee4f4b90f84192a19874cd
Ananth Subramaniam <ansubramania@nvidia.com>
2025-09-29 21:00:58 -0700

    update
    Signed-off-by: Ananth Subramaniam <ansubramania@nvidia.com>

--- scripts/performance/run_script.py
+++ scripts/performance/run_script.py
@@ -167,6 +167,7 @@ def main():
     pretrain(config=recipe, forward_step_func=forward_step)
 
     if torch.distributed.is_initialized():
+        torch.distributed.barrier()
         torch.distributed.destroy_process_group()
 
 
