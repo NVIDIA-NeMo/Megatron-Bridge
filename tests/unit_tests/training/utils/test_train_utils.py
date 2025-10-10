20c74b7a56d693df2b73b39f232076af3ca492b1
dimapihtar <dpihtar@gmail.com>
2025-10-10 07:12:09 -0700

    fix style
    Signed-off-by: dimapihtar <dpihtar@gmail.com>

--- tests/unit_tests/training/utils/test_train_utils.py
+++ tests/unit_tests/training/utils/test_train_utils.py
@@ -21,7 +21,6 @@ import numpy as np
 import pytest
 import torch
 
-
 from megatron.bridge.training.state import GlobalState
 from megatron.bridge.training.utils.train_utils import (
     maybe_inject_state,
