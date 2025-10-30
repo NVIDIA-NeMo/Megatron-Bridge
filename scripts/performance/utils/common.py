c9b08b22fa4abd951ec9c342fc693611be5a416c
mollys <mollys@mollys.nvidia.com>
2025-10-30 16:21:10 -0700

    Move model specific defaults from common to yamls

--- scripts/performance/utils/common.py
+++ scripts/performance/utils/common.py
@@ -27,40 +27,13 @@ def get_perf_matrix_overrides(yaml_root: Any, args: Any) -> Any:
     gpu_block = perf.get(args.gpu) or {}
     preset = gpu_block.get(num_gpus_yaml_key) or {}
 
-    # weak scaling for deepseek
-    if preset == {} and args.model_name in ["deepseek"]:
-        default_num_gpus = 1024 if args.gpu.lower() in ["h100"] else 256
+    if preset == {} and args.model_name in ["deepseek", "llama3", "llama31"]:
+        defaults = yaml_root.get("defaults")
+        yaml_gpu_defaults = defaults.get("gpu_defaults")
+        default_num_gpus = yaml_gpu_defaults.get(args.gpu) or yaml_gpu_defaults.get(args.gpu.lower())
         num_gpus_yaml_key = f"num_gpus_{default_num_gpus}"
         preset = gpu_block.get(num_gpus_yaml_key)
-        preset["common"]["gbs"] = args.num_gpus * 8
-
-    elif preset == {} and args.model_name in ["llama3", "llama31"]:
-        gpu_defaults = {
-            "gb300": {
-                "405b": 128,
-                "70b":  64,
-                "8b":   8,
-            },
-            "gb200": {
-                "405b": 128,
-                "70b":  64,
-                "8b":   8,
-            },
-            "b200": {
-                "405b": 128,
-                "70b":  64,
-                "8b":   8,
-            },
-            "h100": {
-                "405b": 1024,
-                "70b":  64,
-                "8b":   8,
-            },
-        }
-        default_num_gpus = gpu_defaults[args.gpu][args.model_size]
-        num_gpus_yaml_key = f"num_gpus_{default_num_gpus}"
-        preset = gpu_block.get(num_gpus_yaml_key)
-        scaling_factor = preset["common"]["gbs"] / default_num_gpus
+        scaling_factor = defaults.get("gbs_scaling_factor_default")
         preset["common"]["gbs"] = int(args.num_gpus * scaling_factor)
 
     if args.tensor_parallel_size: preset["common"]["tp"] = args.tensor_parallel_size
