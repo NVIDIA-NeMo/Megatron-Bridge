1c1f4655e65fc22cc37ac46ccaa98611d6d7c04a
Malay Nagda <malayn@nvidia.com>
2025-11-08 20:26:35 +0530

    correct arg list
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/perf_plugins.py
+++ scripts/performance/perf_plugins.py
@@ -357,12 +357,12 @@ class PerfEnvPlugin(Plugin):
 
     def setup(self, task: Union["run.Partial", "run.Script"], executor: "run.Executor"):
         """Enable the performance environment settings"""
-        parallelism_defaults = get_workload_base_config(
-            self.model_name, self.model_size, self.gpu, self.num_gpus, self.compute_dtype, self.fp8_recipe
+        workload_base_config = get_workload_base_config(
+            self.model_name, self.model_size, self.gpu, self.compute_dtype, self.fp8_recipe
         )
-        tp_size = (self.tp_size if self.tp_size is not None else parallelism_defaults.tensor_model_parallel_size,)
-        pp_size = (self.pp_size if self.pp_size is not None else parallelism_defaults.pipeline_model_parallel_size,)
-        cp_size = (self.cp_size if self.cp_size is not None else parallelism_defaults.context_parallel_size,)
+        tp_size = (self.tp_size if self.tp_size is not None else workload_base_config.tensor_model_parallel_size,)
+        pp_size = (self.pp_size if self.pp_size is not None else workload_base_config.pipeline_model_parallel_size,)
+        cp_size = (self.cp_size if self.cp_size is not None else workload_base_config.context_parallel_size,)
 
         # Force program order kernel launch for TP, CP overlap
         enable_deepep = self.gpu in ["h100"] and self.model_name == "deepseek" and self.model_size == "v3"
