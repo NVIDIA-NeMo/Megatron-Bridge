9feb470aa4617e5dcbe057bb5e4e591269890c1d
oliver könig <okoenig@nvidia.com>
2025-11-10 13:58:28 +0000

    fix
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- scripts/performance/utils/helpers.py
+++ scripts/performance/utils/helpers.py
@@ -135,7 +135,7 @@ def set_cuda_graph_overrides(
             recipe.rng.te_rng_tracker = recipe.model.use_te_rng_tracker = False
 
     if cuda_graph_impl == "transformer_engine":
-        assert cuda_graph_scope == ["full"] or cuda_graph_scope == ["attn"], (
+        assert cuda_graph_scope == ["full"] or cuda_graph_scope == ["attn"] or cuda_graph_scope == ["mlp"], (
             f"Invalid cuda graph scope: {cuda_graph_scope}. Valid options are: full, attn"
         )
 
