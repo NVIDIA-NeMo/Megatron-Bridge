8bf06316835fd7a95a5e2fe7dc7db99598a73f42
Lawrence Lane <llane@nvidia.com>
2025-11-14 11:23:02 -0500

    exclude venv
    Signed-off-by: Lawrence Lane <llane@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", "**/.venv"]
 
 # -- Options for MyST Parser (Markdown) --------------------------------------
 # MyST Parser settings
