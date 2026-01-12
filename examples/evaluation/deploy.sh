c869b8871329201b060d57e9294f10fa4eac9a1f
oliver könig <okoenig@nvidia.com>
2026-01-12 11:19:09 +0000

    move scripts
    Signed-off-by: oliver könig <okoenig@nvidia.com>

---
+++ examples/evaluation/deploy.sh
@@ -0,0 +1,16 @@
+# Unset SLURM/PMI/PMIX env vars to prevent MPI initialization issues
+for i in $(env | grep ^SLURM_ | cut -d"=" -f 1); do unset -v $i; done
+for i in $(env | grep ^PMI_ | cut -d"=" -f 1); do unset -v $i; done
+for i in $(env | grep ^PMIX_ | cut -d"=" -f 1); do unset -v $i; done
+
+python \
+  /opt/Export-Deploy/scripts/deploy/nlp/deploy_ray_inframework.py \
+  --megatron_checkpoint /lustre/fsw/coreai_dlalgo_llm/malayn/nemo_models/checkpoints/llama3_8b_megatron/iter_0000000/  \
+  --model_id megatron_model \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --num_gpus 8 \
+  --num_replicas 1 \
+  --tensor_model_parallel_size 1 \
+  --pipeline_model_parallel_size 1 \
+  --context_parallel_size 1
