0ef7b413fbe0487a79a7820e0729e212e1b8e28f
Malay Nagda <malayn@nvidia.com>
2026-01-14 17:45:19 +0530

    squad packed args
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/llama/llama3_llm_finetune.py
+++ scripts/performance/configs/llama/llama3_llm_finetune.py
@@ -112,7 +112,7 @@ def llama3_70b_sft_config_gb300(precision: str = "bf16", config_variant: str = "
     cfg = llama3_70b_finetune_config(
         peft="none",
         precision_config=precision_config,
-        packed_sequence=False,
+        packed_sequence=True,
         seq_length=4096,
     )
     set_llama3_common_peft_configs(cfg)
@@ -130,10 +130,10 @@ def llama3_70b_sft_config_gb300(precision: str = "bf16", config_variant: str = "
     cfg.dataset.packed_sequence_specs.pad_cu_seqlens = True
     cfg.dataset.dataset_kwargs["pad_to_max_length"] = True
 
-    if precision == "fp8_mx":  # keeping this eanbled causes NaN grad norm
-        cfg.comm_overlap.overlap_param_gather = False
-        cfg.ddp.overlap_param_gather = False
-        cfg.optimizer.overlap_param_gather = False
+    # if precision == "fp8_mx":  # keeping this eanbled causes NaN grad norm
+    #     cfg.comm_overlap.overlap_param_gather = False
+    #     cfg.ddp.overlap_param_gather = False
+    #     cfg.optimizer.overlap_param_gather = False
 
     return cfg
 
@@ -230,11 +230,11 @@ def llama3_70b_lora_config_gb300(precision: str = "bf16", config_variant: str = 
     cfg.dataset.packed_sequence_specs.pad_cu_seqlens = True
     cfg.dataset.dataset_kwargs["pad_to_max_length"] = True
 
-    if precision == "fp8_mx":  # keeping this eanbled causes NaN grad norm
-        if cfg.comm_overlap is not None and isinstance(cfg.comm_overlap, CommOverlapConfig):
-            cfg.comm_overlap.overlap_param_gather = False
-        cfg.ddp.overlap_param_gather = False
-        cfg.optimizer.overlap_param_gather = False
+    # if precision == "fp8_mx":  # keeping this eanbled causes NaN grad norm
+    #     if cfg.comm_overlap is not None and isinstance(cfg.comm_overlap, CommOverlapConfig):
+    #         cfg.comm_overlap.overlap_param_gather = False
+    #     cfg.ddp.overlap_param_gather = False
+    #     cfg.optimizer.overlap_param_gather = False
 
     return cfg
 
