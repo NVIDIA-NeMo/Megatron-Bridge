ff1cbe05e6cf6223a20ae74a28f0f3bc693b6e79
jinliangl <jinliangl@nvidia.com>
2026-02-06 17:30:39 +0800

    move qwen3vl step into model/qwen3 folder

--- examples/models/vlm/qwen_vl/finetune_qwen_vl.py
+++ examples/models/vlm/qwen_vl/finetune_qwen_vl.py
@@ -103,7 +103,7 @@ from megatron.bridge.training.utils.omegaconf_utils import (
     create_omegaconf_dict_config,
     parse_hydra_overrides,
 )
-from megatron.bridge.training.qwen3vl_step import forward_step as qwen3vl_forward_step
+from megatron.bridge.models.qwen_vl.qwen3_vl_step import forward_step as qwen3_vl_forward_step
 from megatron.bridge.training.vlm_step import forward_step as vlm_forward_step
 from megatron.bridge.utils.common_utils import get_rank_safe
 
@@ -210,7 +210,7 @@ def main() -> None:
     if recipe_name.startswith("qwen3"):
         recipe_module = qwen3_vl_recipes
         model_family = "Qwen3-VL"
-        forward_step_func = qwen3vl_forward_step
+        forward_step_func = qwen3_vl_forward_step
     elif recipe_name.startswith("qwen25"):  # qwen25
         recipe_module = qwen25_vl_recipes
         model_family = "Qwen2.5-VL"
