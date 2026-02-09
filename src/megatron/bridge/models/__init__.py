63c1326cee20a6f7b2d628677f0cc82e9f23c9a5
root <martinzhang0314@gmail.com>
2026-02-09 23:44:42 +0000

    add model registration in init files. add config converter and weight initializer in bridge file. add unit test files
    Signed-off-by: root <martinzhang0314@gmail.com>

--- src/megatron/bridge/models/__init__.py
+++ src/megatron/bridge/models/__init__.py
@@ -130,6 +130,7 @@ from megatron.bridge.models.qwen_omni import (
     Qwen3OmniModelProvider,
 )
 from megatron.bridge.models.qwen_vl import (
+    Qwen25OmniBridge,
     Qwen25VLBridge,
     Qwen25VLModel,
     Qwen25VLModelProvider,
@@ -228,6 +229,7 @@ __all__ = [
     # Nemotron Models
     "NemotronBridge",
     # VL Models
+    "Qwen25OmniBridge",
     "Qwen25VLModel",
     "Qwen25VLBridge",
     "Qwen25VLModelProvider",
--- src/megatron/bridge/models/qwen_vl/__init__.py
+++ src/megatron/bridge/models/qwen_vl/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from megatron.bridge.models.qwen_vl.qwen25_omni_bridge import Qwen25OmniBridge
 from megatron.bridge.models.qwen_vl.modeling_qwen25_vl import Qwen25VLModel
 from megatron.bridge.models.qwen_vl.modelling_qwen3_vl.model import Qwen3VLModel
 from megatron.bridge.models.qwen_vl.qwen3_vl_bridge import Qwen3VLBridge, Qwen3VLMoEBridge
@@ -28,6 +29,7 @@ from megatron.bridge.models.qwen_vl.qwen35_vl_provider import Qwen35VLModelProvi
 
 
 __all__ = [
+    "Qwen25OmniBridge",
     "Qwen25VLModel",
     "Qwen25VLBridge",
     "Qwen25VLModelProvider",
