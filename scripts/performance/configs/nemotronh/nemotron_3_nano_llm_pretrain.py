17bfadb4846a26dbee473df01bd1553b12e43178
Malay Nagda <malayn@nvidia.com>
2026-03-03 16:32:01 +0530

    no grouped gemm
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/nemotronh/nemotron_3_nano_llm_pretrain.py
+++ scripts/performance/configs/nemotronh/nemotron_3_nano_llm_pretrain.py
@@ -76,6 +76,9 @@ def nemotron_3_nano_pretrain_config_gb200(
     if base_cfg.moe_flex_dispatcher_backend is not None:
         cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
 
+    if precision == "nvfp4":
+        cfg.model.moe_grouped_gemm = False
+
     return cfg
 
 
@@ -99,6 +102,9 @@ def nemotron_3_nano_pretrain_config_b300(
     set_workload_base_configs(cfg, base_cfg)
     if base_cfg.moe_flex_dispatcher_backend is not None:
         cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
+
+    if precision == "nvfp4":
+        cfg.model.moe_grouped_gemm = False
 
     return cfg
 
