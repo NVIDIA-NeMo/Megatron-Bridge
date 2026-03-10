f675c1e16789c50ec7931b85d387a73458923bf0
Huy Vu2 <huvu@login-eos02.eos.clusters.nvidia.com>
2026-03-10 08:43:17 -0700

    just keep apply_rope_fusion for now'

--- src/megatron/bridge/diffusion/models/wan/wan_provider.py
+++ src/megatron/bridge/diffusion/models/wan/wan_provider.py
@@ -54,7 +54,7 @@ class WanModelProvider(TransformerConfig, ModelProviderMixin[VisionModule]):  # 
     bf16: bool = False
     params_dtype: torch.dtype = torch.float32
     qkv_format: str = "thd"  # "sbhd". NOTE: if we use context parallelism, we need to use "thd"
-    apply_rope_fusion: bool = True
+    apply_rope_fusion: bool = False # currently, in Megatron-LM's TE, apply_rope_fusion + thd doesn't support interleaved RoPE
     bias_activation_fusion: bool = True
     # these attributes are unused for images/videos, we just set because bridge training requires for LLMs
     seq_length: int = 1024
