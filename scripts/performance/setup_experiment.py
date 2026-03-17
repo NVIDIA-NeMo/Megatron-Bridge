0db42d6f9fcf5d5d264d5cb2218c05dd94ac8461
Malay Nagda <malayn@nvidia.com>
2026-03-17 22:15:33 +0530

    profiling_trace_fix- all precisions
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/setup_experiment.py
+++ scripts/performance/setup_experiment.py
@@ -369,10 +369,10 @@ def main(
         )
 
     if enable_nsys:
-        if compute_dtype == "fp8_mx" and nsys_trace is None:
-            logger.warning("Using `cuda-sw` trace mode for MXFP8 profiling")
-            logger.warning("MXFP8 profiling results might not be accurate due to software tracing limitations.")
-            # TODO: Remove this once the functional issues associated with PyT 26.02 are resolved.
+        if nsys_trace is None:
+            logger.warning("Using `cuda-sw` trace mode for profiling")
+            logger.warning("Profiling results might not be accurate due to software tracing limitations.")
+            # TODO: Remove this once the associated functional issues are resolved.
             nsys_trace = ["cuda-sw", "nvtx"]
         plugins.append(
             NsysPlugin(
