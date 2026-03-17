a7753703ad2e4b8c78220a138eacafcc14316046
jinliangl <jinliangl@nvidia.com>
2026-03-16 23:35:42 -0700

    add default value in kimi provider

--- examples/models/vlm/kimi_vl/sft.sh
+++ examples/models/vlm/kimi_vl/sft.sh
@@ -3,10 +3,9 @@ export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 torchrun --nproc_per_node=8 scripts/training/run_recipe.py \
     --recipe kimi_k25_vl_sft_config \
-    --dataset_type hf \
     --step_func vlm_step \
     --hf_path moonshotai/Kimi-K2.5 \
-    model.seq_length=4096 \
+    model.seq_length=512 \
     model.num_layers=2 \
     model.hidden_size=7168 \
     model.ffn_hidden_size=1024 \
@@ -25,3 +24,4 @@ torchrun --nproc_per_node=8 scripts/training/run_recipe.py \
     train.global_batch_size=4 \
     dataset.maker_name=make_cord_v2_dataset \
     dataset.pack_sequences_in_batch=false \
+    dataset.seq_length=512 \
