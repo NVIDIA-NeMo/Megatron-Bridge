a26bdbba4451b74911eb80a5e573123137e68e83
Malay Nagda <malayn@nvidia.com>
2026-03-20 20:01:54 +0530

    revert dsv3, qw 30b_a3b
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
+++ scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
@@ -94,7 +94,7 @@ DEEPSEEK_V3_PRETRAIN_CONFIG_B300_V1 = replace(
     pipeline_model_parallel_size=16,
     expert_model_parallel_size=8,
     global_batch_size=2048,
-    recompute_modules=["moe_act"],
+    recompute_modules=["mla_up_proj"],
     moe_flex_dispatcher_backend="hybridep",
     moe_a2a_overlap=False,
 )
@@ -173,7 +173,7 @@ DEEPSEEK_V3_PRETRAIN_CONFIG_B300_BF16_V2 = replace(
     DEEPSEEK_V3_PRETRAIN_CONFIG_B300_V2,
     pipeline_model_parallel_size=8,
     virtual_pipeline_model_parallel_size=None,
-    recompute_modules=["moe_act"],
+    recompute_modules=["mla_up_proj"],
 )
 DEEPSEEK_V3_PRETRAIN_CONFIG_B300_FP8_CS_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B300_V2
 DEEPSEEK_V3_PRETRAIN_CONFIG_B300_FP8_MX_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B300_FP8_CS_V2
