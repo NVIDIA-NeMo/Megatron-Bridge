f1532663dc331ada432480f94ab8736b496c27f8
oliver könig <okoenig@nvidia.com>
2026-03-21 15:22:04 +0000

    update for 0.3.1
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ import sys
 project = "Megatron Bridge"
 copyright = "2026, NVIDIA Corporation"
 author = "NVIDIA Corporation"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
