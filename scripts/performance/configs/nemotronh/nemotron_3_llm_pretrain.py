90d8d3b4603df2bdf0921a610e69d5652bd6f922
Malay Nagda <malayn@nvidia.com>
2026-03-31 15:11:14 +0530

    more recompute b200
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/nemotronh/nemotron_3_llm_pretrain.py
+++ scripts/performance/configs/nemotronh/nemotron_3_llm_pretrain.py
@@ -141,9 +141,9 @@ def nemotron_3_super_pretrain_config_b200(
     if base_cfg.moe_flex_dispatcher_backend is not None:
         cfg.model.moe_flex_dispatcher_backend = base_cfg.moe_flex_dispatcher_backend
 
-    if precision != "nvfp4":
-        cfg.model.fine_grained_activation_offloading = True
-        cfg.model.offload_modules = ["core_attn", "qkv_linear"]
+    # if precision != "nvfp4":
+    #     cfg.model.fine_grained_activation_offloading = True
+    #     cfg.model.offload_modules = ["core_attn", "qkv_linear"]
 
     return cfg
 
