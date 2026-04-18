5acef9309caa26d4be918e50f9597e4b83f3efdd
malay-nagda <malayn@nvidia.com>
2026-04-18 06:57:30 +0530

    b200 DSv3 better cfg (#3368)
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
+++ scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
@@ -219,8 +219,29 @@ DEEPSEEK_V3_PRETRAIN_CONFIG_B200_V2 = replace(
 )
 DEEPSEEK_V3_PRETRAIN_CONFIG_B200_BF16_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B200_V2
 DEEPSEEK_V3_PRETRAIN_CONFIG_B200_FP8_CS_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B200_V2
-DEEPSEEK_V3_PRETRAIN_CONFIG_B200_FP8_MX_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B200_FP8_CS_V2
-DEEPSEEK_V3_PRETRAIN_CONFIG_B200_NVFP4_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_B200_V2
+DEEPSEEK_V3_PRETRAIN_CONFIG_B200_FP8_MX_V2 = replace(
+    DEEPSEEK_V3_PRETRAIN_CONFIG_B200_FP8_CS_V2,
+    moe_flex_dispatcher_backend="deepep",
+    pipeline_model_parallel_size=8,
+    virtual_pipeline_model_parallel_size=2,
+    expert_model_parallel_size=32,
+    moe_a2a_overlap=True,
+    pp_layout="Et*4|(t*4|)*14tmL",
+    cuda_graph_impl="transformer_engine",
+    cuda_graph_scope=["attn", "moe_router", "moe_preprocess"],
+    recompute_modules=["mla_up_proj", "mlp"],
+)
+DEEPSEEK_V3_PRETRAIN_CONFIG_B200_NVFP4_V2 = replace(
+    DEEPSEEK_V3_PRETRAIN_CONFIG_B200_V2,
+    moe_flex_dispatcher_backend="deepep",
+    pipeline_model_parallel_size=8,
+    virtual_pipeline_model_parallel_size=2,
+    expert_model_parallel_size=32,
+    moe_a2a_overlap=True,
+    pp_layout="Et*4|(t*4|)*14tmL",
+    cuda_graph_impl="none",
+    recompute_modules=["mla_up_proj", "layernorm", "moe_act"],
+)
 
 
 DEEPSEEK_V3_PRETRAIN_CONFIG_H100_V2 = replace(
