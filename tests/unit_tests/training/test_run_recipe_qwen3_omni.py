5182158f7fddbbe4e3b051fa8a53cc11d709b09e
hbhflw2000 <417911774@qq.com>
2026-04-22 20:17:40 +0800

    Resolve upstream conflicts for omni training wiring
    Signed-off-by: hbhflw2000 <417911774@qq.com>

--- tests/unit_tests/training/test_run_recipe_qwen3_omni.py
+++ tests/unit_tests/training/test_run_recipe_qwen3_omni.py
@@ -32,6 +32,19 @@ def _load_run_recipe_module():
     module_name = "test_run_recipe_qwen3_omni_module"
 
     recipe_config = object()
+    megatron_module = types.ModuleType("megatron")
+    bridge_module = types.ModuleType("megatron.bridge")
+    models_module = types.ModuleType("megatron.bridge.models")
+    qwen_omni_models_module = types.ModuleType("megatron.bridge.models.qwen_omni")
+    qwen_vl_models_module = types.ModuleType("megatron.bridge.models.qwen_vl")
+    diffusion_module = types.ModuleType("megatron.bridge.diffusion")
+    diffusion_models_module = types.ModuleType("megatron.bridge.diffusion.models")
+    flux_models_module = types.ModuleType("megatron.bridge.diffusion.models.flux")
+    wan_models_module = types.ModuleType("megatron.bridge.diffusion.models.wan")
+    training_module = types.ModuleType("megatron.bridge.training")
+    training_utils_module = types.ModuleType("megatron.bridge.training.utils")
+    recipes_utils_module = types.ModuleType("megatron.bridge.recipes.utils")
+
     recipes_module = types.ModuleType("megatron.bridge.recipes")
     recipes_module.qwen3_omni_30b_a3b_sft_config = lambda **_: recipe_config
 
@@ -50,6 +63,24 @@ def _load_run_recipe_module():
     llava_step = types.ModuleType("megatron.bridge.training.llava_step")
     llava_step.forward_step = object()
 
+    audio_lm_step = types.ModuleType("megatron.bridge.training.audio_lm_step")
+    audio_lm_step.forward_step = object()
+
+    flux_step = types.ModuleType("megatron.bridge.diffusion.models.flux.flux_step")
+
+    class FluxForwardStep:
+        pass
+
+    flux_step.FluxForwardStep = FluxForwardStep
+
+    wan_step = types.ModuleType("megatron.bridge.diffusion.models.wan.wan_step")
+
+    class WanForwardStep:
+        def __init__(self, mode=None):
+            self.mode = mode
+
+    wan_step.WanForwardStep = WanForwardStep
+
     finetune_module = types.ModuleType("megatron.bridge.training.finetune")
     finetune_module.finetune = Mock(name="finetune")
 
@@ -62,10 +93,33 @@ def _load_run_recipe_module():
     omegaconf_module = types.ModuleType("megatron.bridge.training.utils.omegaconf_utils")
     omegaconf_module.process_config_with_overrides = lambda config, cli_overrides=None: config
 
+    dataset_utils_module = types.ModuleType("megatron.bridge.recipes.utils.dataset_utils")
+    dataset_utils_module.DATASET_TYPES = ["llm-finetune", "llm-pretrain-mock"]
+    dataset_utils_module.infer_mode_from_dataset = lambda dataset: "finetune"
+    dataset_utils_module.apply_dataset_override = (
+        lambda config, dataset_type, packed_sequence, seq_length, cli_overrides: config
+    )
+
     stub_modules = {
+        "megatron": megatron_module,
+        "megatron.bridge": bridge_module,
+        "megatron.bridge.models": models_module,
+        "megatron.bridge.models.qwen_omni": qwen_omni_models_module,
+        "megatron.bridge.models.qwen_vl": qwen_vl_models_module,
+        "megatron.bridge.diffusion": diffusion_module,
+        "megatron.bridge.diffusion.models": diffusion_models_module,
+        "megatron.bridge.diffusion.models.flux": flux_models_module,
+        "megatron.bridge.diffusion.models.wan": wan_models_module,
+        "megatron.bridge.training": training_module,
+        "megatron.bridge.training.utils": training_utils_module,
         "megatron.bridge.recipes": recipes_module,
+        "megatron.bridge.recipes.utils": recipes_utils_module,
+        "megatron.bridge.recipes.utils.dataset_utils": dataset_utils_module,
+        "megatron.bridge.diffusion.models.flux.flux_step": flux_step,
+        "megatron.bridge.diffusion.models.wan.wan_step": wan_step,
         "megatron.bridge.models.qwen_omni.qwen3_omni_step": qwen3_omni_step,
         "megatron.bridge.models.qwen_vl.qwen3_vl_step": qwen3_vl_step,
+        "megatron.bridge.training.audio_lm_step": audio_lm_step,
         "megatron.bridge.training.gpt_step": gpt_step,
         "megatron.bridge.training.vlm_step": vlm_step,
         "megatron.bridge.training.llava_step": llava_step,
@@ -132,10 +186,10 @@ class TestRunRecipeQwen3Omni:
         original_argv = sys.argv
         sys.argv = [
             "run_recipe.py",
-            "--mode",
-            "finetune",
             "--recipe",
             "qwen3_omni_30b_a3b_sft_config",
+            "--dataset",
+            "llm-finetune",
             "--step_func",
             "qwen3_omni_step",
         ]
