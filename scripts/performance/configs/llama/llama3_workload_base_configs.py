739b4d689f29e96e35824efc05bc202e055a65f2
Malay Nagda <malayn@nvidia.com>
2026-04-28 18:23:18 +0530

    pp2, vp 20
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/llama/llama3_workload_base_configs.py
+++ scripts/performance/configs/llama/llama3_workload_base_configs.py
@@ -591,8 +591,8 @@ _LLAMA3_70B_LORA_CONFIG_GB300 = replace(
 LLAMA3_70B_LORA_CONFIG_GB300_BF16_V1 = _LLAMA3_70B_LORA_CONFIG_GB300
 LLAMA3_70B_LORA_CONFIG_GB300_FP8_CS_V1 = replace(
     _LLAMA3_70B_LORA_CONFIG_GB300,
-    pipeline_model_parallel_size=1,
-    # virtual_pipeline_model_parallel_size=20,
+    pipeline_model_parallel_size=2,
+    virtual_pipeline_model_parallel_size=20,
 )
 LLAMA3_70B_LORA_CONFIG_GB300_FP8_MX_V1 = replace(
     _LLAMA3_70B_LORA_CONFIG_GB300,
