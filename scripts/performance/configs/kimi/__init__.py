62923f0f8073e7411df98d8d43274fe2cec4624a
Bhargav <beede@nvidia.com>
2026-05-29 17:17:17 +0530

    Adding B300 config for Kimi K2 (#4055)
    Signed-off-by: Bhargav Eede <beede@nvidia.com>

--- scripts/performance/configs/kimi/__init__.py
+++ scripts/performance/configs/kimi/__init__.py
@@ -22,12 +22,16 @@ except ModuleNotFoundError:
 if HAVE_MEGATRON_BRIDGE:
     from .kimi_llm_pretrain import (
         kimi_k2_pretrain_config_b200,
+        kimi_k2_pretrain_config_b300,
         kimi_k2_pretrain_config_gb200,
         kimi_k2_pretrain_config_gb300,
         kimi_k2_pretrain_config_h100,
     )
 
 from .kimi_workload_base_configs import (
+    KIMI_K2_PRETRAIN_CONFIG_B300_BF16,
+    KIMI_K2_PRETRAIN_CONFIG_B300_FP8_CS,
+    KIMI_K2_PRETRAIN_CONFIG_B300_FP8_MX,
     KIMI_K2_PRETRAIN_CONFIG_B200_BF16,
     KIMI_K2_PRETRAIN_CONFIG_B200_FP8_CS,
     KIMI_K2_PRETRAIN_CONFIG_B200_FP8_MX,
@@ -45,6 +49,9 @@ from .kimi_workload_base_configs import (
 
 
 __all__ = [
+    "KIMI_K2_PRETRAIN_CONFIG_B300_BF16",
+    "KIMI_K2_PRETRAIN_CONFIG_B300_FP8_CS",
+    "KIMI_K2_PRETRAIN_CONFIG_B300_FP8_MX",
     "KIMI_K2_PRETRAIN_CONFIG_B200_BF16",
     "KIMI_K2_PRETRAIN_CONFIG_B200_FP8_CS",
     "KIMI_K2_PRETRAIN_CONFIG_B200_FP8_MX",
@@ -65,6 +72,7 @@ if HAVE_MEGATRON_BRIDGE:
         [
             "kimi_k2_pretrain_config_gb300",
             "kimi_k2_pretrain_config_gb200",
+            "kimi_k2_pretrain_config_b300",
             "kimi_k2_pretrain_config_b200",
             "kimi_k2_pretrain_config_h100",
         ]
