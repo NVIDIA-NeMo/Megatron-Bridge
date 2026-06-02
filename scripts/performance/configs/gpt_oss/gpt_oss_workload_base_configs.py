b567375e0608fe7f07c799823233eb48014ebcc1
Malay Nagda <malayn@nvidia.com>
2026-06-02 17:25:27 +0530

    more recompute
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/gpt_oss/gpt_oss_workload_base_configs.py
+++ scripts/performance/configs/gpt_oss/gpt_oss_workload_base_configs.py
@@ -201,7 +201,6 @@ GPT_OSS_120B_PRETRAIN_CONFIG_H100_BF16_V2 = replace(
 GPT_OSS_120B_PRETRAIN_CONFIG_GB300_FP8_MX_V2 = replace(
     GPT_OSS_120B_PRETRAIN_CONFIG_GB300_BF16_V2,
     expert_model_parallel_size=1,
-    pipeline_model_parallel_size=2,
     micro_batch_size=1,
     cuda_graph_impl="full_iteration",
     cuda_graph_scope=[],
@@ -209,8 +208,9 @@ GPT_OSS_120B_PRETRAIN_CONFIG_GB300_FP8_MX_V2 = replace(
     moe_a2a_overlap=False,
     cutedsl_fused_grouped_mlp=True,
     fp8_dot_product_attention=True,
-    # recompute_modules=[],
+    recompute_modules=["moe_act", "moe", "layernorm", "core_attn"],
 )
+
 GPT_OSS_120B_PRETRAIN_CONFIG_GB200_FP8_MX_V2 = replace(
     GPT_OSS_120B_PRETRAIN_CONFIG_GB200_BF16_V2,
     expert_model_parallel_size=32,
