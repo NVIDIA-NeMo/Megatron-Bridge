a6b8a31439a8a6abaee6afa163092b9e2edd5c6b
Malay Nagda <malayn@nvidia.com>
2026-06-04 14:23:05 +0530

    1f1b
    Signed-off-by: Malay Nagda <malayn@nvidia.com>

--- scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
+++ scripts/performance/configs/deepseek/deepseek_workload_base_configs.py
@@ -201,9 +201,10 @@ DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_BF16_V2 = replace(
 DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_FP8_CS_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_V2
 DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_FP8_MX_V2 = replace(
     DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_V2,
-    moe_a2a_overlap=False,
+    moe_a2a_overlap=True,
     cutedsl_fused_grouped_mlp=True,
     fp8_dot_product_attention=True,
+    recompute_modules=["mla_up_proj", "mlp"],
 )
 DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_NVFP4_V2 = DEEPSEEK_V3_PRETRAIN_CONFIG_GB200_V2
 
