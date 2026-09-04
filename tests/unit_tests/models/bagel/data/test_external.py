aa28bf2670bd5257fc2cc5725f7cf292cda0e380
yaoyu-33 <yaoyu.094@gmail.com>
2026-09-04 08:57:56 -0700

    [test] fix: add BAGEL test copyright headers
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- tests/unit_tests/models/bagel/data/test_external.py
+++ tests/unit_tests/models/bagel/data/test_external.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+
 import pytest
 from megatron.core.rerun_state_machine import RerunDataIterator
 
--- tests/unit_tests/models/bagel/data/test_order.py
+++ tests/unit_tests/models/bagel/data/test_order.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
+
 from pathlib import Path
 
 import pytest
