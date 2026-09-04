58bca4577bd23ed5bc3810b8310de1c5c0def3e1
Yu Yao <yaoyu.094@gmail.com>
2026-09-04 03:17:01 -0700

    [data] fix: Preserve RNG state during synchronous loading
    Signed-off-by: Yu Yao <yaoyu.094@gmail.com>

--- tests/unit_tests/data/test_samplers.py
+++ tests/unit_tests/data/test_samplers.py
@@ -1,5 +1,8 @@
 # Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
 
+import random
+
+import numpy as np
 import pytest
 import torch
 from torch.utils.data import Dataset
@@ -108,3 +111,37 @@ def test_cyclic_sampler_resume_seeds_worker_dataset_for_current_epoch(
     expected_value = torch.randint(0, 1_000_000, (1,), generator=generator).item()
 
     assert actual_value.item() == expected_value
+
+
+def test_random_seed_dataset_preserves_main_process_rng_state() -> None:
+    """Synchronous dataset seeding must not overwrite the training RNG streams."""
+    dataset = RandomSeedDataset(_RandomValueDataset(), seed=100)
+    dataloader = build_pretraining_data_loader(
+        dataset=dataset,
+        consumed_samples=0,
+        dataloader_type="cyclic",
+        micro_batch_size=1,
+        num_workers=0,
+        data_sharding=False,
+    )
+
+    random.seed(999)
+    np.random.seed(999)
+    torch.manual_seed(999)
+    data_iterator = iter(dataloader)
+
+    python_generator = random.Random()
+    python_generator.setstate(random.getstate())
+    expected_python = python_generator.random()
+    numpy_generator = np.random.RandomState()
+    numpy_generator.set_state(np.random.get_state())
+    expected_numpy = numpy_generator.random_sample()
+    torch_generator = torch.Generator()
+    torch_generator.set_state(torch.random.get_rng_state())
+    expected_torch = torch.rand(1, generator=torch_generator)
+
+    next(data_iterator)
+
+    assert random.random() == expected_python
+    assert np.random.random_sample() == expected_numpy
+    assert torch.equal(torch.rand(1), expected_torch)
