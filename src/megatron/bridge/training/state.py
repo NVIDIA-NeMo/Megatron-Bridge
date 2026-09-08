431738b7ef517294c6ac881d299023516423371a
Dmytro Pykhtar <dpykhtar@nvidia.com>
2026-09-08 13:20:40 +0300

    fix code style
    Signed-off-by: Dmytro Pykhtar <dpykhtar@nvidia.com>

--- src/megatron/bridge/training/state.py
+++ src/megatron/bridge/training/state.py
@@ -38,6 +38,7 @@ from megatron.bridge.training.utils.log_utils import safe_serialize
 from megatron.bridge.training.utils.sig_utils import DistributedSignalHandler
 from megatron.bridge.utils.common_utils import get_rank_safe, get_world_size_safe
 
+
 HAVE_NVRX = has_nvrx_async_support()
 
 
