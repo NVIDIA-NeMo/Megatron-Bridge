b7f49b1bb34cf99729a2ba32b2268254d6ad7de5
Dmytro Pykhtar <dpykhtar@nvidia.com>
2026-09-10 10:09:29 +0300

    fix nemotron config
    Signed-off-by: Dmytro Pykhtar <dpykhtar@nvidia.com>

--- src/megatron/bridge/recipes/nemotronh/h100/nemotron_3_ultra.py
+++ src/megatron/bridge/recipes/nemotronh/h100/nemotron_3_ultra.py
@@ -97,6 +97,7 @@ def nemotron_3_ultra_pretrain_24gpu_h100_bf16_config() -> ConfigContainer:
     cfg.checkpoint.ckpt_assume_constant_structure = True
     cfg.checkpoint.dist_ckpt_strictness = "log_all"
     cfg.checkpoint.async_save = True
+    cfg.checkpoint.async_write_results_mp_mode = "spawn"
 
     cfg.ddp.overlap_grad_reduce = True
     cfg.ddp.overlap_param_gather = True
@@ -305,6 +306,7 @@ def nemotron_3_ultra_sft_192gpu_h100_bf16_openmathinstruct2_packed_config() -> C
     cfg.checkpoint.dist_ckpt_strictness = "log_all"
     cfg.checkpoint.ckpt_assume_constant_structure = True
     cfg.checkpoint.async_save = True
+    cfg.checkpoint.async_write_results_mp_mode = "spawn"
 
     cfg.logger.log_interval = 1
     cfg.rng.seed = 5678
